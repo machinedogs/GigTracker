8ec20d5594ac1242b0cdcd854e2acae10773a923
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-27 18:26:39 -0400

    Added ReduxThunk to the store so we can dispatch async actions in our action creator functions

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import GigTrackerNavigator from './navigation/GigTrackerNavigator';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import eventsReducer from './store/reducers/events'
 import userEventsReducer from './store/reducers/userEvents';
+import authReducer from './store/reducers/auth';
 
 const rootReducer = combineReducers({
   events: eventsReducer,
-  userEvents: userEventsReducer
+  userEvents: userEventsReducer,
+  auth: authReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
