de8e0af700997424642a3cd02cd02d32704465e8
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2020-06-07 15:01:16 -0400

    Update Colors.js

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,7 +2,7 @@ export default {
     primary: '#C2185B',
     accent: '#FFC107',
     lightBackground: '#ecf0f1',
-    lightText: '#fff'
+    lightText: '#fff',
     purpleButton: '#341f97',
     purpleBackground: '#2c2c54'
 };
