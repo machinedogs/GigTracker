e88435ba57099276493089f722fc731fc698cf34
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-06-13 13:55:11 -0400

    removed duplicate key prop from custom pin componenet

--- components/CustomMarker.js
+++ components/CustomMarker.js
@@ -1,18 +1,13 @@
 import React, { Component, useState, useEffect, } from 'react';
 import { Image } from 'react-native';
 
-const PoliticsPin = require('../assets/Pins/PoliticsPin.png');
-const SportsPin = require('../assets/Pins/SportsPin.png');
-const MeetingPin = require('../assets/Pins/MeetingPin.png');
-const FoodPin = require('../assets/Pins/FoodPin.png');
-const ProtestPin = require('../assets/Pins/ProtestPin.png');
-const PartyPin = require('../assets/Pins/PartyPin.png');
-const MusicPin = require('../assets/Pins/MusicPin.png');
-
 const PinMarker = (props) => {
+
     const [extraData, setExtraData] = useState(false);
     useEffect(() => { setExtraData(true); }, []);
+
     let pinIcon;
+    
     if (props.category === "music") {
         pinIcon = require('../assets/Pins/MusicPin.png');
         console.log("Marker Category: " + props.category)
@@ -28,7 +23,6 @@ const PinMarker = (props) => {
     return (
         <Image
             style={{ maxHeight: 75, maxWidth: 75 }}
-            key={`${extraData}`}
             // check for platform here, this is because with android we need to force a rerender
             key={Platform.OS === 'android' ? `${extraData}` : null}
             source={pinIcon}
