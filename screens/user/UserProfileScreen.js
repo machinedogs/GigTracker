5c30e6d567982babc98a1da19197e15f10f841e6
Dalvir Singh <dalvir.singh@cerner.com>
2020-06-12 22:30:03 -0400

    updated layout

--- screens/user/UserProfileScreen.js
+++ screens/user/UserProfileScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useReducer, useCallback } from "react";
 import {
+	Image,
+	ImageBackground,
+	Platform,
 	StyleSheet,
 	Text,
 	View,
 	TouchableOpacity,
-	Image,
 	Alert,
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,11 +15,12 @@ import * as ImagePicker from "expo-image-picker";
 import { updateUserProfile } from "../../store/actions/user";
 import * as firebase from "firebase";
 import { firebaseConfig } from "../../firebase/index";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
 const UserProfileScreen = (props) => {
 	const dispatch = useDispatch();
 	var profileImage = useSelector((state) => state.user.profileImage);
+	var user = useSelector((state) => state.user);
 
 	//Function that lets you pick the image from your library
 	let openImagePickerAsync = async () => {
@@ -60,37 +63,44 @@ const UserProfileScreen = (props) => {
 			.getDownloadURL();
 		console.log(`Got image from firebase. here it is.... ${url}`);
 		//Save uri to storage
-		saveDataToStorage(url)
+		saveDataToStorage(url);
 		return url;
 	};
 
-	//Saves profile uri to local machine storage 
+	//Saves profile uri to local machine storage
 	const saveDataToStorage = async (profileImage) => {
-		console.log(`Save to storage got the uri....${profileImage}...securely storing it`)
-		SecureStore.setItemAsync('images', JSON.stringify({
-			profileImage: profileImage
-		})
-	);
-	}
+		console.log(
+			`Save to storage got the uri....${profileImage}...securely storing it`
+		);
+		SecureStore.setItemAsync(
+			"images",
+			JSON.stringify({
+				profileImage: profileImage,
+			})
+		);
+	};
 	return (
 		<View style={styles.container}>
-			<View style={styles.header}>
-				<Image
-					style={styles.wallpaper}
+			<View style={styles.headerContainer}>
+				<ImageBackground
+					style={styles.headerBackgroundImage}
+					blurRadius={10}
 					source={{
 						uri:
 							"https://visme.co/blog/wp-content/uploads/2017/07/50-Beautiful-and-Minimalist-Presentation-Backgrounds-018.jpg",
 					}}
-				/>
-				<TouchableOpacity
-					onPress={openImagePickerAsync}
-					style={styles.avatarContainer}
 				>
-				<Image style={styles.avatar} source={{ uri: profileImage }} />
-				</TouchableOpacity>
-
-				<Text>Name</Text>
-				
+					<View style={styles.headerColumn}>
+						<TouchableOpacity
+							onPress={openImagePickerAsync}
+							style={styles.userImageContainer}
+						>
+							<Image style={styles.userImage} source={{ uri: profileImage }} />
+						</TouchableOpacity>
+						<Text style={styles.userNameText}>{user.userName}</Text>
+						<Text style={styles.emailText}>{user.userEmail}</Text>
+					</View>
+				</ImageBackground>
 			</View>
 			<View style={styles.content}>
 				<Tabs>
@@ -103,45 +113,68 @@ const UserProfileScreen = (props) => {
 };
 
 const styles = StyleSheet.create({
-	container: {
-		width: "100%",
-		height: "100%",
+	cardContainer: {
+		backgroundColor: "#FFF",
+		borderWidth: 0,
 		flex: 1,
+		margin: 0,
+		padding: 0,
 	},
-	text:{
-		position: 'absolute',
-		fontFamily: 'Cochin'
-	},
-	wallpaper: {
-		height: "100%",
-		width: "100%"
-	},
-	header: {
-		height: "25%",
-	},
-	contentHeader: {
-		marginTop: 50,
+	container: {
+		flex: 1,
 	},
 	content: {
 		width: "100%",
 		height: "100%",
 	},
-	avatarContainer: {
-		backgroundColor: "#696969",
-		width: 130,
-		height: 130,
-		borderRadius: 63,
-		borderWidth: 4,
-		borderColor: "white",
-		marginBottom: 10 ,
-		alignSelf: "center",
-		position: "absolute",
-		marginTop: 20,
+	headerBackgroundImage: {
+		paddingBottom: 20,
+		paddingTop: 35,
 	},
-	avatar: {
-		width: "100%",
+	headerContainer: {},
+	headerColumn: {
+		backgroundColor: "transparent",
+		...Platform.select({
+			ios: {
+				alignItems: "center",
+				elevation: 1,
+				marginTop: -1,
+			},
+			android: {
+				alignItems: "center",
+			},
+		}),
+	},
+	scroll: {
+		backgroundColor: "#FFF",
+	},
+	userImageContainer: {
+		borderColor: "#FFFFFF",
+		borderRadius: 85,
+		borderWidth: 4,
+		height: 170,
+		marginBottom: 15,
+		width: 170,
+	},
+	userImage: {
+		borderColor: "#A5A5A5",
+		borderRadius: 85,
 		height: "100%",
-		borderRadius: 63,
+		width: "100%",
+	},
+	userNameText: {
+		color: "#FFF",
+		fontSize: 22,
+		fontWeight: "bold",
+		paddingBottom: 8,
+		textAlign: "center",
+	},
+	emailText: {
+		color: "#FFF",
+		fontSize: 14,
+		fontWeight: "bold",
+		paddingBottom: 8,
+		textAlign: "center",
 	},
 });
 export default UserProfileScreen;
