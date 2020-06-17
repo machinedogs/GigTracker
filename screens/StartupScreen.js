a72a689c13820bafc1bb75f3c3cd63b0ddf88a67
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-06-17 17:53:21 -0400

    Added try catch to refresh dispatch in startup. When the catch happens we delete invalid data from SecureStore and navigate to home screen

--- screens/StartupScreen.js
+++ screens/StartupScreen.js
@@ -45,7 +45,13 @@ const StartupScreen = props => {
             // check if access token expired, then make refresh endpoint call
             if (accessTokenExpiryDate <= new Date()) {
                 console.log('refreshing tokens')
-                await dispatch(authActions.refresh(userEmail, userName, refreshToken));
+                try {
+                    await dispatch(authActions.refresh(userEmail, userName, refreshToken));
+                } catch (error) {
+                    props.navigation.navigate('Home');
+                    return;
+                }
+
                 //Dispatch action to update profile image state in store 
                 await dispatch(updateUserProfile(profileImage, transformedData))
                 props.navigation.navigate('Home');
