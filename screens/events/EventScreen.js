2e7baf8f2852788e4ced023a64a9fde3043dbe9e
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-07-03 16:46:36 -0400

    Added logic for going button in Event Screen

--- screens/events/EventScreen.js
+++ screens/events/EventScreen.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import React, { useState } from 'react';
 import { Thumbnail, Right, Left } from 'native-base';
 import { Icon } from 'react-native-elements';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image, Dimensions, Button } from 'react-native';
 import { Col, Grid } from 'react-native-easy-grid';
 import InsetShadow from 'react-native-inset-shadow';
 
@@ -26,7 +26,9 @@ const EventScreen = (props) => {
     } else {
         initialEventSaveState = false;
     }
+
     const [isEventSaved, setEventSaved] = useState(initialEventSaveState);
+    const [isGoing, setGoing] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -43,94 +45,114 @@ const EventScreen = (props) => {
     };
 
     return (
-
-        <ScrollView style={{ backgroundColor: 'white' }} showsVerticalScrollIndicator={false}>
-            <View style={{ flexDirection: 'row', padding: 15 }}>
-                <View style={styles.titleDescriptionContainer}>
-                    <Text style={styles.titleText}>{event.title}</Text>
-                </View>
-                <View style={{ alignSelf: 'flex-end' }}>
-                    <View style={styles.hostContent}>
-                        <Thumbnail source={{ uri: event.host.profile }} />
-                        <Text style={styles.hostNameText}>{event.host.name}</Text>
+            <ScrollView style={{ backgroundColor: 'white' }} showsVerticalScrollIndicator={false}>
+                <View style={{ flexDirection: 'row', padding: 15 }}>
+                    <View style={styles.titleDescriptionContainer}>
+                        <Text style={styles.titleText}>{event.title}</Text>
+                    </View>
+                    <View style={{ alignSelf: 'flex-end' }}>
+                        <View style={styles.hostContent}>
+                            <Thumbnail source={{ uri: event.host.profile }} />
+                            <Text style={styles.hostNameText}>{event.host.name}</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-            <Grid>
+                <Grid>
 
-                <Col size={1} style={{ height: 275, paddingHorizontal: 15 }}>
-                    <InsetShadow>
-                        <Image
-                            style={{ flex: 1 }}
-                            source={{ uri: event.image }}
-                        />
-                    </InsetShadow>
+                    <Col size={1} style={{ height: 275, paddingHorizontal: 15 }}>
+                        <InsetShadow>
+                            <Image
+                                style={{ flex: 1 }}
+                                source={{ uri: event.image }}
+                            />
+                        </InsetShadow>
 
-                </Col>
-            </Grid>
-            <View style={{ flexDirection: 'row', paddingHorizontal: 15, paddingTop: 10, paddingBottom: 15 }}>
-                <Left size={2} style={{ height: 'auto' }}>
-                    <Text style={{ fontSize: 15, color: 'black', marginTop: 10, margin: 5 }}>
-                        {makeFullAddress(event.location.address)}
-                    </Text>
-                </Left>
-                <Right size={2} style={{ height: 'auto' }}>
-                    {Platform.OS === 'ios' ?
-                        (
-                            <Text style={{ fontSize: 35, color: 'black' }}>
-                                {new Date(event.date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true })}
-                            </Text>
-
-                        ) :
-                        (
-                            // format time for android
-                            <Text style={{ fontSize: 35, color: 'black' }}>
-                                {formatStandardTime(event.date)}
-                            </Text>
-                        )}
-
-                    <Text style={{ fontSize: 20, color: 'black', paddingLeft: 10 }}>
-                        {new Date(event.date).toLocaleDateString()}
-                    </Text>
-                </Right>
-            </View>
-            <Grid>
-                <Col size={1} style={{ height: 'auto' }}>
-                    <InsetShadow shadowRadius={1} shadowColor='black' left={false} right={false} shadowOpacity={1}>
-                        <Text style={styles.Description}>
-                            {event.description}
+                    </Col>
+                </Grid>
+                <View style={{ flexDirection: 'row', paddingHorizontal: 15, paddingTop: 10, paddingBottom: 15 }}>
+                    <Left size={2} style={{ height: 'auto', justifyContent: 'center' }}>
+                        <Text style={{ fontSize: 15, color: 'black' }}>
+                            {makeFullAddress(event.location.address)}
                         </Text>
-                    </InsetShadow>
-                </Col>
-            </Grid>
-            <Grid>
-                {(userName != event.host.name && userName) ? // make sure user is not the host and is logged in
-                    (<Col>
-                        <TouchableOpacity onPress={toggleSaveButton} style={{ marginTop: 10 }} >
+                    </Left>
+                    <Right size={2} style={{ height: 'auto' }}>
+                        {Platform.OS === 'ios' ?
+                            (
+                                <Text style={{ fontSize: 35, color: 'black' }}>
+                                    {new Date(event.date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true })}
+                                </Text>
+
+                            ) :
+                            (
+                                // format time for android
+                                <Text style={{ fontSize: 35, color: 'black' }}>
+                                    {formatStandardTime(event.date)}
+                                </Text>
+                            )}
+
+                        <Text style={{ fontSize: 20, color: 'black' }}>
+                            {new Date(event.date).toLocaleDateString()}
+                        </Text>
+                    </Right>
+                </View>
+                <Grid>
+                    <Col size={1} style={{ height: 'auto', justifyContent: 'center' }}>
+                        <InsetShadow shadowRadius={1} shadowColor='black' left={false} right={false} shadowOpacity={1}>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingVertical: 15, paddingHorizontal: 15 }}>
+                                <Text style={styles.goingText}>
+                                    50 People Going
+                            </Text>
+                                <TouchableOpacity>
+                                    <View style={{
+                                        backgroundColor: isGoing ? '#f5b800' : Colors.lightBackground,
+                                        borderRadius: 5,
+                                        borderColor: isGoing ? '#f5b800' : Colors.lightBackground,
+                                        borderWidth: 2,
+                                        paddingHorizontal: 10
+                                    }}>
+                                        <Button
+                                            title={isGoing ? 'Going' : 'Going'}
+                                            color='black'
+                                            onPress={() => { setGoing(!isGoing) }}
+                                        />
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                        </InsetShadow>
+                    </Col>
+                </Grid>
+                <View style={{ padding: 15 }}>
+                    <Text style={styles.Description}>
+                        {event.description}
+                    </Text>
+                </View>
+                <Grid>
+                    {(userName != event.host.name && userName) ? // make sure user is not the host and is logged in
+                        (<Col>
+                            <TouchableOpacity onPress={toggleSaveButton} style={{ marginTop: 10, marginBottom: 10 }} >
+                                <Icon
+                                    name='save'
+                                    type='font-awesome'
+                                    size={40}
+                                    color={isEventSaved ? '#f5b800' : 'black'}
+                                />
+                                <Text style={styles.ButtonText}>{isEventSaved ? 'Unsave Event' : 'Save Event'}</Text>
+                            </TouchableOpacity>
+                        </Col>) : null
+                    }
+                    <Col size={1} style={{ width: 75 }}>
+                        <TouchableOpacity style={{ marginTop: 10, marginBottom: 10 }}>
                             <Icon
-                                name='save'
+                                name='share-alt'
                                 type='font-awesome'
                                 size={40}
-                                color={isEventSaved ? '#f5b800' : 'black'}
+                                color={'black'}
                             />
-                            <Text style={styles.ButtonText}>{isEventSaved ? 'Unsave Event' : 'Save Event'}</Text>
+                            <Text style={styles.ButtonText}>Share Event</Text>
                         </TouchableOpacity>
-                    </Col>) : null
-                }
-                <Col size={1} style={{ width: 75 }}>
-                    <TouchableOpacity style={{ marginTop: 10 }}>
-                        <Icon
-                            name='share-alt'
-                            type='font-awesome'
-                            size={40}
-                            color={'black'}
-                        />
-                        <Text style={styles.ButtonText}>Share Event</Text>
-                    </TouchableOpacity>
-                </Col>
-            </Grid>
-        </ScrollView >
-
+                    </Col>
+                </Grid>
+            </ScrollView >
     );
 }
 
@@ -182,12 +204,12 @@ const styles = StyleSheet.create({
     Description: {
         color: 'black',
         backgroundColor: 'white',
-        marginVertical: 2,
-        marginLeft: 0,
-        marginRight: 0,
-        paddingHorizontal: 20,
-        paddingVertical: 17,
         fontSize: 17
+    },
+    goingText: {
+        color: 'black',
+        backgroundColor: 'white',
+        fontSize: 20
     }
 });
 
