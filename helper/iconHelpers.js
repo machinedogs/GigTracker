1d5693e89c975ffd2aeddb73b7437eb53ee167c2
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-07-23 18:18:36 -0400

    added extra pin catagories and redesigned pins

--- helper/iconHelpers.js
+++ helper/iconHelpers.js
@@ -5,10 +5,14 @@ const FoodPin = require('../assets/Pins/FoodPin.png');
 const ProtestPin = require('../assets/Pins/ProtestPin.png');
 const PartyPin = require('../assets/Pins/PartyPin.png');
 const MusicPin = require('../assets/Pins/MusicPin.png');
+const MarketPin = require('../assets/Pins/MarketPin.png')
+const DiscussionPin = require('../assets/Pins/DiscussionPin.png')
+const OtherPin = require('../assets/Pins/OtherPin.png')
+const ArtPin = require('../assets/Pins/ArtPin.png')
 
 export const iconPicker = (category) => {
     let pinIcon;
-    
+
     if (category === "music") {
         pinIcon = MusicPin;
         console.log("Marker Category: " + category)
@@ -29,6 +33,22 @@ export const iconPicker = (category) => {
         console.log("Marker Category: " + category)
     } else if (category === "protest") {
         pinIcon = ProtestPin;
+        console.log("Marker Category: " + category)
+    }
+    else if (category === "art") {
+        pinIcon = ArtPin;
+        console.log("Marker Category: " + category)
+    }
+    else if (category === "market") {
+        pinIcon = MarketPin;
+        console.log("Marker Category: " + category)
+    }
+    else if (category === "discussion") {
+        pinIcon = DiscussionPin;
+        console.log("Marker Category: " + category)
+    }
+    else if (category === "other") {
+        pinIcon = OtherPin;
         console.log("Marker Category: " + category)
     }
 
