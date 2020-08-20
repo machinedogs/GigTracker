9aa27cea7e88363b9030590d50290f595b58c3c7
John Murray <johnmurray@temple.edu>
2020-08-20 13:39:57 -0400

    added light theme

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import { UserNavigator, GuestNavigator } from './GigTrackerNavigator';
 import StartupScreen from '../screens/StartupScreen';
+import Colors from '../constants/Colors';
+
+const LightTheme = {
+  ...DefaultTheme,
+  colors = {
+    ...DefaultTheme.colors,
+    primary: Colors.purpleButton,
+    background: Colors.darkGrey
+  }
+}
 
 const AppNavigator = props => {
   const isAuth = useSelector(state => state.user.accessToken);
   const didTryAutoLogin = useSelector(state => state.user.didTryAutoLogin);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={LightTheme}>
       {isAuth.length > 0 && didTryAutoLogin === true && <UserNavigator />}
       {isAuth.length < 1 && didTryAutoLogin === true && <GuestNavigator />}
       {isAuth.length < 1 && didTryAutoLogin === false && <StartupScreen />}
